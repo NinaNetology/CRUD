db72d5ef04708dbc38326c05b8abfa6aea497070
NinaNetology <151594929+NinaNetology@users.noreply.github.com>
2025-01-30 18:11:57 +0500

    Update serializers.py

--- stocks_products/logistic/serializers.py
+++ stocks_products/logistic/serializers.py
@@ -1,9 +1,42 @@
-from rest_framework.routers import DefaultRouter
+from rest_framework import serializers
 
-from logistic.views import ProductViewSet, StockViewSet
+from logistic.models import Product, Stock, StockProduct
 
-router = DefaultRouter()
-router.register('products', ProductViewSet)
-router.register('stocks', StockViewSet)
 
-urlpatterns = router.urls
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = Product
+        fields = ['id', 'title', 'description']
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta():
+        model = Stock
+        fields = ['id', 'address', 'positions']
+
+    def create(self, validated_data):
+        positions = validated_data.pop('positions')
+        stock = super().create(validated_data)
+
+        for position in positions:
+            StockProduct.objects.create(stock=stock, **position)
+
+        return stock
+
+    def update(self, instance, validated_data):
+        positions = validated_data.pop('positions')
+        stock = super().update(instance, validated_data)
+
+        for position in positions:
+            product = position.pop('product')
+            StockProduct.objects.update_or_create(stock=stock, product=product, defaults={**position})
+
+        return stock
